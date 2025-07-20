9f80691e7a0d86fce66f30d375f22bb45fd22cb9
mandos1995 <mandos19950620@gmail.com>
2025-07-20 15:12:30 +0900

    Chore 프로젝트 생성

---
+++ Project.swift
@@ -0,0 +1,34 @@
+import ProjectDescription
+
+let project = Project(
+    name: "HealthStamp",
+    targets: [
+        .target(
+            name: "HealthStamp",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "com.mandos.HealthStamp",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchScreen": [
+                        "UIColorName": "",
+                        "UIImageName": "",
+                    ],
+                ]
+            ),
+            sources: ["HealthStamp/Sources/**"],
+            resources: ["HealthStamp/Resources/**"],
+            dependencies: []
+        ),
+        .target(
+            name: "HealthStampTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "com.mandos.HealthStampTests",
+            infoPlist: .default,
+            sources: ["HealthStamp/Tests/**"],
+            resources: [],
+            dependencies: [.target(name: "HealthStamp")]
+        ),
+    ]
+)
